6252f6003061283a0b67556ef555edfec241d3a8
RutaUrbonaviciute <ruta.urbonaviciute@hotmail.com>
2020-05-21 10:34:57 +0300

    Setup startRemoveExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,14 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, removeExpense, editExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import {
+  startAddExpense,
+  addExpense,
+  removeExpense,
+  editExpense,
+  setExpenses,
+  startSetExpenses,
+  startRemoveExpense
+} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -19,6 +27,22 @@ test('should setup remove expense action object', () => {
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: '123abc'
+  })
+})
+
+test('should remove expenses from firebase', (done) => {
+  const store = createMockStore({})
+  const id = expenses[0].id
+  store.dispatch(startRemoveExpense({ id })).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'REMOVE_EXPENSE',
+      id
+    })
+    return database.ref(`expenses/${id}`).once('value').then((snapshot) => {
+      expect(snapshot.val()).toBeFalsy()
+      done()
+    })
   })
 })
 
@@ -109,3 +133,4 @@ test('should fetch expenses from database', (done) => {
     done()
   })
 })
+
--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,15 +3,15 @@ import { shallow } from 'enzyme';
 import { EditExpensePage } from '../../components/EditExpensePage';
 import expenses from '../fixtures/expenses';
 
-let removeExpense, editExpense, history, wrapper
+let startRemoveExpense, editExpense, history, wrapper
 
 beforeEach(() => {
-  removeExpense = jest.fn()
+  startRemoveExpense = jest.fn()
   editExpense = jest.fn()
   history = {
     push: jest.fn()
   }
-  wrapper = shallow(<EditExpensePage editExpense={editExpense} removeExpense={removeExpense} history={history} expense={expenses[1]} />)
+  wrapper = shallow(<EditExpensePage editExpense={editExpense} startRemoveExpense={startRemoveExpense} history={history} expense={expenses[1]} />)
 })
 
 
@@ -19,17 +19,15 @@ test('should render EditExpensePage correctly', () => {
   expect(wrapper).toMatchSnapshot()
 })
 
-test('should hanle edit expense', () => {
+test('should handle edit expense', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
   expect(history.push).toHaveBeenLastCalledWith('/')
   expect(editExpense).toHaveBeenLastCalledWith(expenses[1].id, expenses[1])
 })
 
 
-test('should handle remove expense', () => {
+test('should handle startremove expense', () => {
   wrapper.find('button').simulate('click')
   expect(history.push).toHaveBeenLastCalledWith('/')
-  expect(removeExpense).toHaveBeenLastCalledWith({
-    id: expenses[1].id
-  })
+  expect(startRemoveExpense).toHaveBeenLastCalledWith(expenses[1].id)
 })
