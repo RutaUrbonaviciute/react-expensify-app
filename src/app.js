915ee38d9881cd10924eb9cf3e3c3703b20237e5
RutaUrbonaviciute <ruta.urbonaviciute@hotmail.com>
2020-05-22 13:25:51 +0300

    Fix firebase related warnings by changing firebase imports

--- src/app.js
+++ src/app.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 import { login, logout } from './actions/auth';
 import { startSetExpenses } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses'
+import 'react-dates/initialize';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
